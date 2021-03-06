9fc35810a18053afb1d54a6be440558a19082a46
Roberto Maia <romaiajr5@gmail.com>
2021-03-06 12:07:44 -0300

    Lexema Maker only split in lines now

--- lexema_maker.py
+++ lexema_maker.py
@@ -1,50 +1,15 @@
 class LexemaMaker():
    
     def makeLexema(self,code):
-        lexemas = []
-        for line in code.splitlines(): # Dividir código fonte em linhas
-            lexemas+= self.__splitSpaces(line)
+        lexemas = code.splitlines()
+        # for line in code.splitlines(): # Dividir código fonte em linhas
+        #     lexemas+= self.__splitSpaces(line)
         return lexemas
         
-    def __splitSpaces(self,line):
-        withoutSpaces = line.replace(" ", "\s") # Substituir espaços por \s
-        return withoutSpaces.split("\s")# Separar em lexemas de acordo com o \s
+    # def __splitSpaces(self,line):
+    #     withoutSpaces = line.replace(" ", "\s") # Substituir espaços por \s
+    #     return withoutSpaces.split("\s")# Separar em lexemas de acordo com o \s
 
 if __name__ == "__main__": 
     lex = LexemaMaker()
-    print(lex.makeLexema("Para Testar o Objeto"))
-
-# # import re
-# # code = re.sub(' +', ' ', code)
-# code = "test for    this \"test for    this\""
-# print(code)
-# def replace_space(text):
-#     counter = 0
-#     firstSpace = ''
-#     openedString = False
-#     for i in range(len(code)):
-#         character = code[i]
-#         isAspas = character == "\""
-#         if isAspas:
-#             if openedString:
-#                 openedString = False
-#             openedString = True
-#         elif character.isspace():
-#             if firstSpace == '':
-#                 firstSpace = i
-#             elif openedString:
-#                 firstSpace = ''
-#             else:
-#                 newCode = code[:firstSpace] + code[i:]
-#                 counter+=1
-#                 firstSpace = i-counter
-#         elif not character.isspace():
-#             # if firstSpace != '':
-#             #     if not openedString:
-#             #         newCode = code[:firstSpace] +"\s"+ code[i:]
-#             firstSpace = ''
-#     return newCode
-
-# code = replace_space(code)
-# print(code)
-#lexemas = []
+    print(lex.makeLexema("\"teste de string \n Roberto"))
