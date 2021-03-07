7e7889285c6329124b6b0781ac8fbc9b51256ac5
Roberto Maia <romaiajr5@gmail.com>
2021-03-07 19:21:31 -0300

    Classify Lexemas Almost Finished

--- classify_lexema.py
+++ classify_lexema.py
@@ -3,10 +3,10 @@ class ClassifyLexema():
     "function", "start", "return", "if", "else", "then", "while", "read","print", "int",
     "real", "boolean", "string", "true", "false",
     "global", "local"]
+    delimitadores = [";",",","(",")","{","}","[","]","."]
     operadorAritmetico = ["+","-","*","/","++","--"]
     operadoresRelacionais = ["==","!=",">",">=","<","<=","="]
     operadorLogico = ["&&","||","!"]
-    delimitadores = [";",",","(",")","{","}","[","]","."]
     symbolsTable = []
     
     def getToken(self,lexema): 
@@ -20,34 +20,33 @@ class ClassifyLexema():
             if charList[counter].isspace(): # Verifica se é um espaço
                 counter += 1
             elif charList[counter].isidentifier(): # Verifica se é um caracter
-                while charList[counter].isidentifier():
+                while charList[counter].isidentifier() or charList[counter].isnumeric():
                     word += charList[counter]
                     counter += 1
                     if counter==length:
                         break    
-                if self.reservedWords.__contains__(word):
-                    tokens.append("<Palavra Reservada, "+word+">")
-                else:
-                    tokens.append("<ID, "+word+">")
                 if not self.symbolsTable.__contains__(word):
                     self.symbolsTable.append(word)
+                if self.reservedWords.__contains__(word):
+                    tokens.append("<Palavra Reservada, "+word+" >")
+                else:
+                    tokens.append("<ID, "+word+" [" + str(self.symbolsTable.index(word))+"]>") #Token ID REVIEW
                 word = ''                  
             elif charList[counter].isnumeric(): # Verifica se é um número
-                while charList[counter].isnumeric():
+                while charList[counter].isnumeric(): #TODO resolver problema de dígitos
                     word += charList[counter]
                     counter += 1
                     if counter==length:
                         break
                 if word.isnumeric():
-                    tokens.append("<Número, "+word+">")
+                    tokens.append("<Numero, "+word+">")
                 elif word.isdigit():
                     tokens.append("<Digito, "+word+">")
                 word = '' 
             else: # Se não for número, letra ou espaço, será um simbolo
-                if charList[counter]=="\"":#FALTA VER STRINGS DE VARIAS LINHAS
+                if charList[counter]=="\"":# String TODO ver caso de string quebrada em linhas #and charList[counter]!="\n"
                     word+=charList[counter]
                     counter+=1
-                    #and charList[counter]!="\n"
                     while charList[counter]!="\"":
                         word += charList[counter]
                         counter += 1
@@ -56,37 +55,93 @@ class ClassifyLexema():
                     word+=charList[counter]
                     counter+=1
                     tokens.append("<Cadeia de Caracteres>, "+word+">")
-                elif charList[counter]=="/" and charList[counter+1]=="/":
+                elif charList[counter]=="/" and charList[counter+1]=="/": # Comentário em Linha
                     while charList[counter]!="\n":
                         counter+=1
-                elif charList[counter]=="/" and charList[counter+1]=="*":
+                        if counter==length:
+                            break
+                elif charList[counter]=="/" and charList[counter+1]=="*": # Comentário em Bloco TODO rever pq o */ estava bugando
+                    counter+=2
                     while charList[counter]!="*" and charList[counter+1]!="/":
                         counter+=1
+                        if counter==length:
+                            break
+                    counter+=2
+                elif self.delimitadores.__contains__(charList[counter]): # Delimitadores
+                    tokens.append("<Delimitador, "+charList[counter]+">")
+                    counter += 1
+                elif charList[counter] == "+" or charList[counter] == "-" or charList[counter] == "*"  or charList[counter] == "/": # OP. Aritmético
+                    if self.operadorAritmetico.__contains__(charList[counter] + charList[counter+1]):
+                        tokens.append("<Op. Aritmetico, "+ charList[counter] + charList[counter+1] + ">")
+                        counter += 2
+                    else:
+                        tokens.append("<Op. Aritmetico, "+ charList[counter] +">")
+                        counter += 1
+                elif charList[counter] == "=" or charList[counter] == "!" or charList[counter] == ">"  or charList[counter] == "<": # OP. Relacional
+                    if self.operadoresRelacionais.__contains__(charList[counter] + charList[counter+1]):
+                        tokens.append("<Op. Relacional, "+ charList[counter] + charList[counter+1] + ">")
+                        counter += 2
+                    elif self.operadoresRelacionais.__contains__(charList[counter]):
+                        tokens.append("<Op. Relacional, "+ charList[counter] +">")
+                        counter += 1
+                    else:
+                        tokens.append("<Op. Lógico, "+charList[counter]+">")
+                        counter += 1                  
+                elif charList[counter] == "&" or charList[counter] == "|": # OP. Lógico #REVIEW Código mt extenso?
+                    if self.operadorLogico.__contains__(charList[counter] + charList[counter+1]):
+                        tokens.append("<Op. Logico, "+ charList[counter] + charList[counter+1] + ">")
+                        counter += 2
+                    # elif self.operadorLogico.__contains__(charList[counter]): # Equivale ao !, porém já foi chamado em Op. Relacionais
+                    #     tokens.append("<Op. Lógico, "+ charList[counter] +">")
+                    #     counter += 1
+                    else:
+                        tokens.append("<Simbolo, "+charList[counter]+">")
+                        counter += 1
                 else:
                     word+=charList[counter]
                     tokens.append("<Simbolo, "+word+">")
                     counter += 1
                 word = ''
-        for token in tokens:
-            print(token)
-        print(self.symbolsTable)
+        # for token in tokens:
+        #     print(token)
+        # print(self.symbolsTable)
         return tokens
-           
+    
+    def getSymbolsTable(self):
+        return self.symbolsTable
+    
     def splitWord(self,lexema)->str:
         return [char for char in lexema]
         # return list(lexema)
 
 if __name__ == "__main__": 
     cl = ClassifyLexema()
-    arquivo = '''if (idade >= 150) print("pode aposenta
-    r kkkk");
+    arquivo = '''/* 
+	teste08 - sem erros
+*/
++9"HELOU MUNDO"
+const int MAX = 10, MAX2 = 50;
+	
+string Mensagem = "Hello world";
+
+procedure start {  // comeca aqui o programa principal 
+
+	int idade; 
+	real salario;
+	string nome;
+
+	print("Digite o nome");
+	read(nome);
+	print("Digite a idade");
+	read(idade);
+	
+	if (idade >= 150) print("pode aposentar kkkk");
 	else {
 		print("vai trabalhar");
 		salario = salario; // hehehe
-        /* 
-        asfasfasf
-        */
-	}
+	}	
+ 
+} // fim start
 	'''
     cl.getToken(arquivo)
     # for linha in arquivo.splitlines():
--- manage_files.py
+++ manage_files.py
@@ -1,13 +1,23 @@
 import os
 from lexema_maker import LexemaMaker
+from classify_lexema import ClassifyLexema
 files = os.listdir("./input/")
 lex = LexemaMaker()
+clex = ClassifyLexema()
+counter = 0
 for file in files:
-    lexemas = [] 
+    # lexemas = [] 
     pathName = open(f"./input/{file}", "r")
     codigoFonte = pathName.read()
     out = open("./output/"+file.replace("entrada", "saida"), "w")
-    lexemas = lex.makeLexema(codigoFonte)
-    for lexema in lexemas:
-        # CHAMADA DO AUTOMATO PRINCIPAL, PASSANDO LEXEMA POR LEXEMA PARA LEITURA POR CARACTER
-        out.write(lexema.strip() +"\n")#
+    # lexemas = lex.makeLexema(codigoFonte)
+    # for lexema in lexemas:
+    #     # CHAMADA DO AUTOMATO PRINCIPAL, PASSANDO LEXEMA POR LEXEMA PARA LEITURA POR CARACTER
+    #     out.write(lexema.strip() +"\n")#
+    out.write("--------------------------\nTOKENS\n--------------------------\n")
+    for token in clex.getToken(codigoFonte):
+        out.write(token + "\n")
+    out.write("--------------------------\n\n\n--------------------------\nTABELA DE SIMBOLOS\n--------------------------\n")
+    for idx, item in enumerate(clex.getSymbolsTable()):
+        out.write("[" + str(idx)+"] " + item + "\n")
+    out.write("--------------------------")
