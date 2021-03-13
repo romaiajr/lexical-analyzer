0c355f30f1c9ec0ed99df66e53ae2489dc4c2f43
romaiajr <romaiajr5@gmail.com>
2021-03-13 10:09:56 -0300

    Generate now classify  Number, String and Comment

--- generate_tokens.py
+++ generate_tokens.py
@@ -21,7 +21,7 @@ class GenerateTokens():
     # def __iter__(self):
     #     return self.itr
             
-    def initialState(self):
+    def initialState(self): #NOTE Funcionando
         self.lexema = ''
         if self.itr.cur == None:
             return self.tokens
@@ -34,14 +34,14 @@ class GenerateTokens():
             if self.itr.cur.isidentifier():
                 return self.ideState()
             elif self.itr.cur.isnumeric():
-                return self.numberState()
+                return self.numberStateA()
             else:
                 return self.symbolState()
         else:
             self.error_tokens.append(Error_Token(OUT_ASCII_ERROR, self.itr.cur, line))
             return self.initialState()
             
-    def ideState(self):
+    def ideState(self): #NOTE Funcionando
         while self.itr.cur.isidentifier() or self.itr.cur.isnumeric() or self.itr.cur == "_":
             self.lexema+=self.itr.cur
             self.itr.next()
@@ -55,19 +55,100 @@ class GenerateTokens():
             self.tokens.append(Token(ID_TOKEN, self.lexema, self.line))
         return self.initialState()                                         
 
-    def numberState(self):
-        pass
-        
-    def symbolState(self):
-        pass
+    def numberStateA(self): #NOTE Funcionando
+        while self.itr.cur.isnumeric():
+            self.lexema+=self.itr.cur
+            self.itr.next()
+            if self.itr.cur == None:
+                break
+        if self.itr.cur == ".":
+            self.lexema+=self.itr.cur
+            return self.numberStateB()
+        else:
+            self.tokens.append(Token(NUMBER_TOKEN, self.lexema, self.line))
+            return self.initialState()
+
+    def numberStateB(self): #NOTE Funcionando
+        self.itr.next()
+        if self.itr.cur.isnumeric():
+            while self.itr.cur.isnumeric():
+                self.lexema+=self.itr.cur
+                self.itr.next()
+                if self.itr.cur == None:
+                    break
+            self.tokens.append(Token(NUMBER_TOKEN, self.lexema, self.line))
+        else:
+            self.error_tokens.append(Error_Token(NUMBER_ERROR, self.lexema, self.line))
+        return self.initialState()
+
+    def symbolState(self): #NOTE Funcionando
+        if self.itr.cur == "\"":
+            self.lexema += self.itr.cur
+            return self.stringState()  
+        elif self.itr.cur == "/":
+            return self.commentState()
+        else:
+            self.itr.next()
+        return self.initialState()
+
+    def stringState(self): #NOTE Funcionando
+        self.itr.next()
+        while self.itr.cur != "\"" and self.itr.cur != "\n":
+            self.lexema+=self.itr.cur
+            self.itr.next()
+            if self.itr.cur == None:
+                break
+        if self.itr.cur == "\"":
+            self.lexema+=self.itr.cur
+            self.tokens.append(Token(STRING_TOKEN, self.lexema, self.line))
+            self.itr.next()
+        else:
+            self.error_tokens.append(Error_Token(STRING_ERROR, self.lexema, self.line))
+            self.line+=1
+        return self.initialState()
     
+    def commentState(self): #NOTE Funcionando
+        self.itr.next()
+        if self.itr.cur == "/":
+            while self.itr.cur != "\n":
+                self.itr.next()
+                if self.itr.cur == None:
+                    break
+            if self.itr.cur == "\n":
+                self.line+=1
+            return self.initialState()
+        elif self.itr.cur == "*":
+            self.lexema += self.itr.prv + self.itr.cur
+            self.itr.next()
+            while self.itr.cur != "*" and self.itr.nxt != "/":
+                self.lexema += self.itr.cur
+                if self.itr.cur == "\n":
+                    self.line+=1
+                self.itr.next()
+                if self.itr.nxt == None:
+                    break
+            if self.itr.nxt == None:
+                self.error_tokens.append(Error_Token(COMMENT_ERROR, self.lexema, self.line))
+            self.itr.next() # Para pular o */ final
+            self.itr.next()     
+            return self.initialState()
+            
+
+                    
+                    
+
     def isAscii(self, char):
         return 32 <= ord(char) <= 126
 
+    def getErrorTokens(self):
+        return self.error_tokens
+
 if __name__ == "__main__":
-    gt = GenerateTokens(" Roberto  AKOSAKOSKOSA print  ")
+    gt = GenerateTokens("/*comentario\n teste ")
     item = gt.initialState()
     for i in item:
+        print(i)
+    for i in gt.getErrorTokens():
         print(i)
     # for item in previous_current_next("Roberto"):
     #     print(item)
