80db5ed4c2b115dc3c64bd774c02515aca6a1b77
romaiajr <romaiajr5@gmail.com>
2021-03-15 15:43:05 -0300

    Removed unused files + Handling errors in main.py

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import os
 from generate_tokens import GenerateTokens
-files = os.listdir("./input/")
-for file in files:
-    # Seleciona todos os arquivos de entrada e, para cada um deles, gera um arquivo de saída
-    pathName = open(f"./input/{file}", "r")
-    codigoFonte = pathName.read()
-    out = open("./output/"+file.replace("entrada", "saida"), "w")
+path = "./input/"
+if os.path.isdir(path):
+    files = os.listdir(path)
+    if len(files) != 0:
+        for file in files:
+            if file.endswith(".txt"):
+                if file.startswith("entrada"):
+                    # Seleciona todos os arquivos de entrada e, para cada um deles, gera um arquivo de saída
+                    pathName = open(f"{path}{file}", "r")
+                    codigoFonte = pathName.read()
+                    out = open("./output/"+file.replace("entrada", "saida"), "w")
 
-    # Gerando e armazenando tokens e erros
-    gtokens = GenerateTokens(codigoFonte)
-    tokens = gtokens.initialState()
-    errors = gtokens.getErrorTokens()
+                    # Gerando e armazenando tokens e erros
+                    gtokens = GenerateTokens(codigoFonte)
+                    tokens = gtokens.initialState()
+                    errors = gtokens.getErrorTokens()
 
-    # Escrevendo no arquivo de saída
-    for token in tokens:
-        out.write(str(token) + "\n")
-    out.write("\n")
-    for error in errors:
-        out.write(str(error) + "\n")
+                    # Escrevendo no arquivo de saída
+                    for token in tokens:
+                        out.write(str(token) + "\n")
+                    out.write("\n")
+                    for error in errors:
+                        out.write(str(error) + "\n")
+                    
+                    if len(errors) > 0:
+                        print (f"SUCESSO: Arquivo {file} foi lido com sucesso! Foram encontrados {len(errors)} erros.")
+                    else:
+                        print(f"SUCESSO: Arquivo {file} foi lido com sucesso! Não foram encontrados erros.")
+                else:
+                    print(f"ALERTA: O arquivo {file} foi ignorado. Nome do arquivo não corresponde ao padrão estabelecido.")
+            else:
+                print(f"ERRO: Formato do arquivo {file} não é suportado.")
+    else:
+        print(f"ERRO: Não foram encontrado arquivos no diretório {path}")
+else:
+    print(f"ERRO: Diretório {path} não encontrado")
